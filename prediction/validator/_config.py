9ce8855211ba7485d34f6a3f24882d5170f7e87a
panthervis <c0087ms1@outlook.com>
2024-08-06 14:11:15 +0000

    Fix: interval to 60

--- prediction/validator/_config.py
+++ prediction/validator/_config.py
@@ -3,6 +3,6 @@ from pydantic_settings import BaseSettings
 
 class ValidatorSettings(BaseSettings):
     # == Scoring ==
-    iteration_interval: int = 600  # Set, accordingly to your tempo.
+    iteration_interval: int = 60  # Set, accordingly to your tempo.
     max_allowed_weights: int = 800  # Query dynamically based on your subnet settings.
     foo: int | None = None  # Anything else that you wish to implement.
